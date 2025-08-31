263ec9ebc3e0558d4ab9e7cced1f6fda763da678
Nate <natevoci@users.noreply.github.com>
2025-09-01 09:30:03 +1000

    Enhance frame processing by adding quarter-size frame support; update Frame and MediaFile classes, and adjust SceneChangeFrameProcessor to utilize quarter Y data for scene change calculations.

--- LogoDetect/Services/Frame.cs
+++ LogoDetect/Services/Frame.cs
@@ -5,18 +5,21 @@ namespace LogoDetect.Services;
 public class Frame
 {
     YData _yData;
+    YData _quarterYData;
     long _timestamp;
 
 
     public YData YData => _yData;
+    public YData QuarterYData => _quarterYData;
 
     public long Timestamp => _timestamp;
 
     public TimeSpan TimeSpan => TimeSpanExtensions.FromTimestamp(_timestamp);
 
-    public Frame(YData yData, long timestamp)
+    public Frame(YData yData, YData quarterYData, long timestamp)
     {
         _yData = yData;
+        _quarterYData = quarterYData;
         _timestamp = timestamp;
     }
 }
